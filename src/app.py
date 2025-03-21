959c35ae11d5de2a2f6bd87e415a9babaf8a6146
khoa <khoatm20@gmail.com>
2025-03-21 11:49:13 +0700

    add email verification

--- src/app.py
+++ src/app.py
@@ -19,6 +19,7 @@ app.config[
 app.config['MAIL_SERVER'] = config['mail']['MAIL_SERVER']
 app.config['MAIL_PORT'] = config['mail']['MAIL_PORT']
 app.config['MAIL_USE_TLS'] = config['mail']['MAIL_USE_TLS']
+app.config['MAIL_USE_SSL'] = config['mail']['MAIL_USE_SSL']
 app.config['MAIL_USERNAME'] = config['mail']['MAIL_USERNAME']
 app.config['MAIL_PASSWORD'] = config['mail']['MAIL_PASSWORD']
 app.config['MAIL_DEFAULT_SENDER'] = config['mail']['MAIL_USERNAME']
@@ -31,27 +32,3 @@ redis_client = redis.StrictRedis(host='localhost', port=config['redis']['REDIS_P
 
 # Initialize SQLAlchemy with the app
 db.init_app(app)
-
-with app.app_context():
-    db.create_all()
-from src.routes.homepage import homepage
-from src.routes.user_management import user_management
-from src.routes.link_management import link_management
-from src.routes.advertisement_management import advertisement_management
-from src.routes.login import login
-from src.routes.register import register
-from src.routes.redirect_short_link import redirect_short_link
-from src.routes.user import user_blueprint
-from src.routes.link import link_blueprint
-from src.routes.mail import mail_blueprint
-from src.routes.advertisement import advertisement_blueprint
-from src.routes.social_media import social_media_blueprint
-# Đăng ký các blueprint
-app.register_blueprint(user_blueprint)
-app.register_blueprint(link_blueprint)
-app.register_blueprint(mail_blueprint)
-app.register_blueprint(advertisement_blueprint)
-app.register_blueprint(social_media_blueprint)
-
-if __name__ == '__main__':
-    app.run(debug=True)
